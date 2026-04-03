2d61533b8bc6b9abeaf633486cca70bdddcdab19
Nivas <nivaskodavatyvc@flyin.com>
2026-04-03 19:41:06 +0530

    missing code

--- app/src/main/kotlin/com/gtr3/byheart/data/local/dao/NoteDao.kt
+++ app/src/main/kotlin/com/gtr3/byheart/data/local/dao/NoteDao.kt
@@ -17,9 +17,6 @@ interface NoteDao {
     fun getNoteByIdFlow(id: Long): Flow<NoteEntity?>
 
     @Query("SELECT * FROM notes WHERE id = :id")
-    fun getNoteByIdFlow(id: Long): Flow<NoteEntity?>
-
-    @Query("SELECT * FROM notes WHERE id = :id")
     suspend fun getNoteById(id: Long): NoteEntity?
 
     @Upsert
